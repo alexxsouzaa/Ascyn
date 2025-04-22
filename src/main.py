0aa0c4a3eb41b6031be8c7e488b1c97078692dbb
alexxsouzaa <alex2804y@gmail.com>
2025-04-22 00:37:32 -0300

    :sparkles: feat: Adicionar animação deslizante para mensagens de feedback

--- src/main.py
+++ src/main.py
@@ -68,7 +68,8 @@ class App(ctk.CTk):
             width=220,
             values=["@#*:. ",
                     "@%#0*=-:. ",
-                    "@%#0B$&*+=-:,. "]
+                    "@%#0B$&*+=-:,. "],
+            state="readonly"
         )
         self.comboBox_caracteres.place(x=21, y=252)
         self.comboBox_caracteres.set("@#*:. ")
@@ -108,8 +109,7 @@ class App(ctk.CTk):
             command=self.text_insert
         )
         self.button_converte.place(x=21, y=430)
-        self.button_converte.configure(state="disabled")
-
+        #self.button_converte.configure(state="disabled")
 
         self.button_salvar = ctk.CTkButton(
             master=self,
@@ -123,7 +123,7 @@ class App(ctk.CTk):
             command=self.save_file
         )
         self.button_salvar.place(x=211, y=430)
-        self.button_salvar.configure(state="disabled")
+        #self.button_salvar.configure(state="disabled")
 
         # Text Box
         self.textbox = ctk.CTkTextbox(
@@ -136,6 +136,21 @@ class App(ctk.CTk):
         self.textbox.place(x=372, y=0)
         self.textbox.configure(state="disabled", wrap="none")
 
+        self.frame_feedback = ctk.CTkFrame(
+            master=self,
+            width=290,
+            height=40,
+            fg_color="#2EB342",
+            bg_color="#343434",
+            corner_radius=8
+        )
+        self.frame_feedback.place(x=41, y=-50)
+        
+        self.label_feedback = ctk.CTkLabel(
+            master=self.frame_feedback,
+            text="Imagem convertida com sucesso!"
+        )
+        self.label_feedback.place(x=50, y=7)
 
         # Altera o curso do mouse
         def on_enter(event):
@@ -171,31 +186,33 @@ class App(ctk.CTk):
                 self.entry_pathFile.configure(state="disabled")
                 self.button_converte.configure(state="normal")
                 self.button_salvar.configure(state="normal")
-                print("Imagem selecionada:", self.caminho_imagem)
-                print(self.switch_inverteCores.get())
                 return self.caminho_imagem
         except Exception as error:
+            self.show_feedback("Erro ao selecionar a imagem", "#FE696B", "#000000") # Aviso que nenhuma imagem foi selecionada
             print("Erro ao selecionar a imagem: ", error)
 
     def text_insert(self):
         try:
             if self.caminho_imagem:
-                ascii_text = ascii_img(self.caminho_imagem, self.comboBox_caracteres.get(), self.switch_inverteCores.get())
-                self.textbox.configure(state="normal")
-                self.textbox.delete("0.0", "end")
-                self.textbox.insert("0.0", ascii_text)
-                self.textbox.configure(state="disabled")
+                ascii_text = ascii_img(self.caminho_imagem, self.comboBox_caracteres.get(), self.switch_inverteCores.get()) # Converte a imagem em Asciis
+                self.textbox.configure(state="normal")  # Ativa a edição de texto do TextBox
+                self.textbox.delete("0.0", "end")   # Apaga o texto do TextBox
+                self.textbox.insert("0.0", ascii_text) # Insere o texto Ascci no TextBox
+                self.textbox.configure(state="disabled") # Desabilitar a edição de texto do TextBox
+                self.show_feedback("Imagem convertida com sucesso!", "#69FE69", "#000000") # Aviso que a imagem foi convertida com sucesso
             else:
+                self.show_feedback("Nenhuma imagem selecionada.", "#FE696B", "#000000") # Aviso que nenhuma imagem foi selecionada
                 print("Nenhuma imagem selecionada.")
         except Exception as error:
-            print("Erro ao converte imagem: ", error)
+                self.show_feedback("Erro ao converter imagem", "#FE696B", "#000000") # Aviso que nenhuma imagem foi selecionada
+                print("Erro ao converte imagem: ", error)
     
     def save_file(self):
         try:
             conteudo = self.textbox.get("0.0", "end").strip()
             if conteudo:
                 extensao = self.comboBox_extensao.get()
-                tipos_arquivo = [("Arquivos ASCII", f"*{extensao}")]
+                tipos_arquivo = [("Arquivo ASCII", f"*{extensao}")]
 
                 file_path = filedialog.asksaveasfilename(
                     defaultextension=extensao,
@@ -205,6 +222,7 @@ class App(ctk.CTk):
                 if file_path:
                     with open(file_path, "w", encoding="utf-8") as file:
                         file.write(conteudo)
+                        self.show_feedback("Arquivo salvo com sucesso!", "#ffffff", "#000000")
                         print(f"Arquivo salvo com sucesso em {file_path}")
                 else:
                     print("Salvamento cancelado")
@@ -212,7 +230,39 @@ class App(ctk.CTk):
                 print("O arquivo não foi convertido")
 
         except Exception as error:
+            self.show_feedback("Erro ao salvar o arquivo!", "#FE696B", "#ffffff")
             print("Erro ao salvar o arquivo: ", error)
+
+    def show_feedback(self, mensagem=str, color=str, txt_color=str):
+        self.label_feedback.configure(text=mensagem, text_color=txt_color)
+        self.frame_feedback.configure(fg_color=color)
+
+        # Centraliza o texto dinamicamente no frame de feedback
+        self.label_feedback.update_idletasks()
+        label_width = self.label_feedback.winfo_reqwidth()
+        frame_width = self.frame_feedback.winfo_width()
+        centered_x = (frame_width - label_width) // 2
+        self.label_feedback.place(x=centered_x, y=7)
+
+        self.frame_feedback.place(x=41, y=-50)
+        self.slide_feedback(y_inicial=-50, y_final=16, subindo=False)
+
+    def slide_feedback(self, y_inicial, y_final, subindo=False, passo=2, delay=10):
+        if not subindo and y_inicial < y_final:
+            y_inicial += passo
+            self.frame_feedback.place(x=41, y=y_inicial)
+            self.after(delay, lambda: self.slide_feedback(y_inicial, y_final, subindo, passo, delay))
+        elif not subindo:
+            # Espera 2 segundos, depois anima pra cima
+            self.after(2000, lambda: self.slide_feedback(y_inicial=y_final, y_final=-50, subindo=True, passo=2, delay=10))
+        elif subindo and y_inicial > y_final:
+            y_inicial -= passo
+            self.frame_feedback.place(x=41, y=y_inicial)
+            self.after(delay, lambda: self.slide_feedback(y_inicial, y_final, subindo, passo, delay))
+        else:
+            self.frame_feedback.place_forget()
+
+
 
 Main = App()
 Main.mainloop()
